b9486e4f53aa0a8c58accc25ef2c056df844e8f7
Amirhanna2001 <amirhanna2001@gmail.com>
2022-07-19 00:39:26 +0200

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using BulkyBookWeb.Controllers.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> allCategories = _db.Categories;
+            return View(allCategories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order And Name Connot Be The Same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id < 1)
+                return NotFound();
+
+            var idFromDb = _db.Categories.Find(id);
+            if (idFromDb == null)
+                return NotFound();
+
+
+            return View(idFromDb);
+
+        }
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var idFromDb = _db.Categories.Find(id);
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(idFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "Category Deleted Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(idFromDb);
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id < 1)
+                return NotFound();
+
+            var idFromDb = _db.Categories.Find(id);
+            if (idFromDb == null)
+                return NotFound();
+
+
+            return View(idFromDb);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order And Name Connot Be The Same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+    }
+}
